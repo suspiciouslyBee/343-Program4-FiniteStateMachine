4b2363926b1ec315236ec219d9d0d15afc18bbb6
suspiciouslyBee <suspiciously@bee.movie>
2025-03-12 14:19:32 -0700

    Works, added basic input handling

--- 343-Program4-FiniteStateMachine/fsm.cpp
+++ 343-Program4-FiniteStateMachine/fsm.cpp
@@ -1,56 +1,55 @@
+#include "fsm.h"
+
 #include <iostream>
 #include <istream>
 #include <string>
 
 #include "state.h"
 
-#include "fsm.h"
-
-//class StateInterface;
+// class StateInterface;
 
 FSM::FSM() {
-
   static Base s1;
   //... declare all
 
-  //starting ptr
+  // starting ptr
   this->currentState_ = &s1;
   return;
 }
 
-void FSM::BeginMachine()
-{
-  while(true) {
-    //Display Current State
-    //Prompt
-    //Show result, repeat
-    //TODO, lock the choices down with checking, maybe a menu selection
+void FSM::BeginMachine() {
+  while (true) {
+    // Display Current State
+    // Prompt
+    // Show result, repeat
+    // TODO, lock the choices down with checking, maybe a menu selection
 
-    int selection = 0;
+    char selection = 0;
 
     std::cout << "Current State: " << currentState_->StateName() << std::endl;
-    std::cout << "Input Action: ";
+    std::cout << "Input Action (1, 2, +, x): ";
 
     std::cin >> selection;
 
     std::cout << "Output: ";
 
-    switch(selection){
-      case 1:
-      currentState_->One(currentState_);
-      break;
-      case 2:
-      currentState_->Two(currentState_);
-      break;
-      case 3:
-      currentState_->Add(currentState_);
-      break;
-      case 4:
-      currentState_->Multiply(currentState_);
-      break;
+    switch (selection) {
+      case '1':
+        currentState_->One(currentState_);
+        break;
+      case '2':
+        currentState_->Two(currentState_);
+        break;
+      case '+':
+        currentState_->Add(currentState_);
+        break;
+      case 'x':
+        currentState_->Multiply(currentState_);
+      default:  // may introduce technically introduce a state
+        std::cout << "\rInput not recognized, reloading state" << std::endl;
+        break;
     }
 
     std::cout << std::endl << std::endl;
-
   }
 }
--- 343-Program4-FiniteStateMachine/main.cpp
+++ 343-Program4-FiniteStateMachine/main.cpp
@@ -5,22 +5,7 @@
 
 #include "fsm.h"
 
-struct FSMNode {
-  FSMNode* operators_[4];
-  char output[4];
-};
-
-/*
-enum STATES {
-  s1,
-  ,
-  2
-};
-*/
-
 int main() { 
   FSM StateMachine;
   StateMachine.BeginMachine();
-
-   
 }
